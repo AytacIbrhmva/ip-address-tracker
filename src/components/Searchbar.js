90ebca58a4e909496cec2b5983110b17daa3d9cf
Aytac <aytacibrahimova2612@gmail.com>
2023-02-05 01:39:15 +0400

    second

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -19,8 +19,8 @@ export default function Searchbar({setIpAddress}) {
   return (
       <form onSubmit={e => search(e)}>
           <input onChange={e => handleChange(e)} type="text" placeholder="Search for any IP address or domain" />
-          <button type="submit" role="button">
-            <img src={ArrowIcon} alt='arrow-icon' />
+          <button type="submit" id="searchButton">
+            <img src={ArrowIcon} alt='arrow icon' />
           </button>
       </form> 
   )
